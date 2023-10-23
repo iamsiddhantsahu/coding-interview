eb4d9d9378a68972541a11e7008e6dc834d16d61
Siddhant Sahu <siddhant_sahu@outlook.com>
2023-10-23 10:08:42 +0200

    graph: network delay time youtube link and examples

--- graph/network-delay-time.py
+++ graph/network-delay-time.py
@@ -9,6 +9,20 @@
 # Return the minimum time it takes for all the n nodes to receive the signal. 
 # If it is impossible for all the n nodes to receive the signal, return -1.
 
+# Example 1:
+# Input: times = [[2,1,1],[2,3,1],[3,4,1]], n = 4, k = 2
+# Output: 2
+
+# Example 2:
+# Input: times = [[1,2,1]], n = 2, k = 1
+# Output: 1
+
+# Example 3:
+# Input: times = [[1,2,1]], n = 2, k = 2
+# Output: -1
+
+# https://www.youtube.com/watch?v=EaphyqKU4PQ
+
 from typing import List
 import collections
 import heapq
